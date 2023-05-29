280062fd287f75c90bdc3aa437c37fb5355c6eec
pratibha <pratibha210>
2023-05-29 17:29:14 +0530

    commit changes in agency

--- src/pages/agency/auth/AgencyDashboard.jsx
+++ src/pages/agency/auth/AgencyDashboard.jsx
@@ -4,6 +4,19 @@ import Loader from "@mui/material/CircularProgress";
 import { agencyAccountDetails, agenciesDashboardData } from "../../../api/api";
 import moment from "moment-timezone";
 import Swal from "sweetalert2";
+import workIcon from "../../../assets/imgs/work.svg";
+import jobsIcon from "../../../assets/imgs/jobs.svg";
+import declinedIcon from "../../../assets/imgs/declined.svg";
+import icon1 from "../../../assets/imgs/icon.png";
+import agency1 from "../../../assets/imgs/agency1.svg";
+import agency2 from "../../../assets/imgs/agency2.svg";
+import agency3 from "../../../assets/imgs/agency3.svg";
+import agency4 from "../../../assets/imgs/agency4.svg";
+import agency5 from "../../../assets/imgs/agency5.svg";
+
+
+
+
 
 function AgencyDashboard() {
   const navigate = useNavigate();
@@ -77,7 +90,7 @@ function AgencyDashboard() {
             <li>
               <div className="dash-box">
                 <div className="icon">
-                  <img src="/src/assets/imgs/work.svg" />
+                  <img src={workIcon} />
                 </div>
                 <h3>No of working on Jobs</h3>
                 <span className="count">
@@ -88,7 +101,7 @@ function AgencyDashboard() {
             <li>
               <div className="dash-box">
                 <div className="icon">
-                  <img src="/src/assets/imgs/jobs.svg" />
+                  <img src={jobsIcon} />
                 </div>
                 <h3>No of Pending Jobs</h3>
                 <span className="count">
@@ -99,7 +112,7 @@ function AgencyDashboard() {
             <li>
               <div className="dash-box">
                 <div className="icon">
-                  <img src="/src/assets/imgs/declined.svg" />
+                  <img src={declinedIcon} />
                 </div>
                 <h3>No of Declined Jobs</h3>
                 <span className="count">
@@ -138,13 +151,13 @@ function AgencyDashboard() {
                       <div className="job-box">
                         <div className="job-header">
                           <div className="logo">
-                            <img src="/src/assets/imgs/icon.png" />
+                            <img src={icon1} />
                           </div>
                           <div className="job-company">
                             <h4>{e?.job?.job_name}</h4>
                             <div className="job-location">
                               <span>
-                                <svg
+                                {/* <svg
                                   width="18"
                                   height="18"
                                   viewBox="0 0 18 18"
@@ -165,7 +178,8 @@ function AgencyDashboard() {
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
                                   ></path>
-                                </svg>
+                                </svg> */}
+                                <img src={agency1} />
                               </span>
                               <h5>{e?.job?.job_location.join(", ")}</h5>
                             </div>
@@ -190,7 +204,7 @@ function AgencyDashboard() {
                           <ul>
                             <li>
                               <span>
-                                <svg
+                                {/* <svg
                                   width="16"
                                   height="16"
                                   viewBox="0 0 16 16"
@@ -201,7 +215,8 @@ function AgencyDashboard() {
                                     d="M8 16C12.4172 16 16 12.4172 16 8C16 3.58285 12.4172 0 8 0C3.58285 0 0 3.58285 0 8C0 12.4172 3.58285 16 8 16ZM7.42855 3.42858C7.42855 3.11428 7.6857 2.85713 8 2.85713C8.3143 2.85713 8.57145 3.11428 8.57145 3.42858V7.72572L11.2143 9.84001C11.46 10.0372 11.5 10.3971 11.3029 10.6429C11.1914 10.7829 11.0257 10.8571 10.8571 10.8571C10.7314 10.8571 10.6057 10.8171 10.5 10.7314L7.64288 8.44573C7.50859 8.33714 7.4286 8.1743 7.4286 8V3.42858H7.42855Z"
                                     fill="#5682ED"
                                   ></path>
-                                </svg>
+                                </svg> */}
+                                <img  src={agency2}/>
                               </span>
                               {moment(e.createdAt).fromNow()}
                             </li>
@@ -211,7 +226,7 @@ function AgencyDashboard() {
                             ) : (
                               <li>
                                 <span>
-                                  <svg
+                                  {/* <svg
                                     width="16"
                                     height="16"
                                     viewBox="0 0 16 16"
@@ -222,7 +237,8 @@ function AgencyDashboard() {
                                       d="M12 1.3999H4C1.6 1.3999 0 2.5999 0 5.3999V10.9999C0 13.7999 1.6 14.9999 4 14.9999H12C14.4 14.9999 16 13.7999 16 10.9999V5.3999C16 2.5999 14.4 1.3999 12 1.3999ZM4.8 12.7999H2.4C2.072 12.7999 1.8 12.5279 1.8 12.1999C1.8 11.8719 2.072 11.5999 2.4 11.5999H4.8C5.128 11.5999 5.4 11.8719 5.4 12.1999C5.4 12.5279 5.128 12.7999 4.8 12.7999ZM8 10.5999C6.67201 10.5999 5.6 9.5279 5.6 8.1999C5.6 6.8719 6.67201 5.7999 8 5.7999C9.328 5.7999 10.4 6.8719 10.4 8.1999C10.4 9.5279 9.328 10.5999 8 10.5999ZM13.6 4.7999H11.2C10.872 4.7999 10.6 4.5279 10.6 4.1999C10.6 3.8719 10.872 3.5999 11.2 3.5999H13.6C13.928 3.5999 14.2 3.8719 14.2 4.1999C14.2 4.5279 13.928 4.7999 13.6 4.7999Z"
                                       fill="#5682ED"
                                     ></path>
-                                  </svg>
+                                  </svg> */}
+                                  <img src={agency3} />
                                 </span>
                                 {e?.job?.min_compensation} - {e?.job?.max_compensation}{" "}
                                 {e?.job?.compensation_type.toUpperCase()}
@@ -231,7 +247,7 @@ function AgencyDashboard() {
                             {e?.job?.candidates?.length > 0 && 
                             <li>
                               <span>
-                                <svg
+                                {/* <svg
                                   width="16"
                                   height="16"
                                   viewBox="0 0 16 16"
@@ -242,14 +258,15 @@ function AgencyDashboard() {
                                     d="M12 1.3999H4C1.6 1.3999 0 2.5999 0 5.3999V10.9999C0 13.7999 1.6 14.9999 4 14.9999H12C14.4 14.9999 16 13.7999 16 10.9999V5.3999C16 2.5999 14.4 1.3999 12 1.3999ZM4.8 12.7999H2.4C2.072 12.7999 1.8 12.5279 1.8 12.1999C1.8 11.8719 2.072 11.5999 2.4 11.5999H4.8C5.128 11.5999 5.4 11.8719 5.4 12.1999C5.4 12.5279 5.128 12.7999 4.8 12.7999ZM8 10.5999C6.67201 10.5999 5.6 9.5279 5.6 8.1999C5.6 6.8719 6.67201 5.7999 8 5.7999C9.328 5.7999 10.4 6.8719 10.4 8.1999C10.4 9.5279 9.328 10.5999 8 10.5999ZM13.6 4.7999H11.2C10.872 4.7999 10.6 4.5279 10.6 4.1999C10.6 3.8719 10.872 3.5999 11.2 3.5999H13.6C13.928 3.5999 14.2 3.8719 14.2 4.1999C14.2 4.5279 13.928 4.7999 13.6 4.7999Z"
                                     fill="#5682ED"
                                   ></path>
-                                </svg>
+                                </svg> */}
+                                <img src={agency4} />
                               </span>
                              {e?.job?.candidates?.length > 0 && e?.job?.candidates?.length + " Active Candidates"}
                             </li>
                             }
                             <li>
                               <span>
-                                <svg
+                                {/* <svg
                                   width="16"
                                   height="16"
                                   viewBox="0 0 16 16"
@@ -260,7 +277,8 @@ function AgencyDashboard() {
                                     d="M12 1.3999H4C1.6 1.3999 0 2.5999 0 5.3999V10.9999C0 13.7999 1.6 14.9999 4 14.9999H12C14.4 14.9999 16 13.7999 16 10.9999V5.3999C16 2.5999 14.4 1.3999 12 1.3999ZM4.8 12.7999H2.4C2.072 12.7999 1.8 12.5279 1.8 12.1999C1.8 11.8719 2.072 11.5999 2.4 11.5999H4.8C5.128 11.5999 5.4 11.8719 5.4 12.1999C5.4 12.5279 5.128 12.7999 4.8 12.7999ZM8 10.5999C6.67201 10.5999 5.6 9.5279 5.6 8.1999C5.6 6.8719 6.67201 5.7999 8 5.7999C9.328 5.7999 10.4 6.8719 10.4 8.1999C10.4 9.5279 9.328 10.5999 8 10.5999ZM13.6 4.7999H11.2C10.872 4.7999 10.6 4.5279 10.6 4.1999C10.6 3.8719 10.872 3.5999 11.2 3.5999H13.6C13.928 3.5999 14.2 3.8719 14.2 4.1999C14.2 4.5279 13.928 4.7999 13.6 4.7999Z"
                                     fill="#5682ED"
                                   ></path>
-                                </svg>
+                                </svg> */}
+                                <img src={agency5}/>
                               </span>
                              {e?.job?.must_have_skills.join(", ")}
                             </li>
